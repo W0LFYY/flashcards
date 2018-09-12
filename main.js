65a7ff81e7e286127b5d58256bb4a7404da726a4
W0LFYY <ethanmadrillrivera@gmail.com>
2018-09-12 16:46:18 -0600

    stuff

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    const flashCards = [ 
+      {
+        question:'samplequestion', answer:'samepleanswer'
+        },
+      {
+        question:'2samplequestion', answer:'2sampleanswer'
+        },
+        {
+        question:'3samplequestion', answer:'3sampleanswer'
+        }
+    ]
+    let randomCard = () => {
+      let flashCard = flashCards[Math.floor(Math.random() * flashCards.length)];
+      $('.question').html(`${flashCard.question}`);
+      $('.answer').html(`${flashCard.answer}`);
+     debugger
+    };
+    
+    
+    $('button').click(function() { 
+      $('p').toggleClass('answer')
+      $('.answer').toggleClass("showBack")
+    })
+    $('.nextCard').click(function(){
+      randomCard()
+      $('p').toggleClass('answer')
+    
+    }
+      
+    )
+    randomCard()})
